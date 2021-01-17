4c5c8ce68aaa0653f1026b182a1d96b187a73218
kir-dev13 <kirill87rabota@gmail.com>
2021-01-18 00:30:14 +0300

    create: hero section, adaptive

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,7 +18,12 @@ testWebP(function (support) {
 ;
 let buttonMobileMenu = document.querySelector(".mobile-btn");
 buttonMobileMenu.addEventListener("click", function () {
-  console.log("клик");
-  document.querySelector(".nav").classList.toggle("nav--visible");
+  let navbarMenu = document.querySelector(".nav");
+  navbarMenu.classList.toggle("nav--visible");
+  if (navbarMenu.classList.contains("nav--visible")) {
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
+  } else {
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+  }
 });
 ;
