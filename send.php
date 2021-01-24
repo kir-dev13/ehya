3a7915c47affbee3ca3f5d4911aeed6d8e63ee0a
kir-dev13 <kirill87rabota@gmail.com>
2021-01-24 23:55:22 +0300

    create: subscribe footer modal adaptive

---
+++ send.php
@@ -0,0 +1,71 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+// $name = $_POST['name'];
+// $phone = $_POST['phone'];
+// $message = $_POST['message'];
+$email = $_POST['email'];
+
+// Формирование самого письма
+$title = "Новая подписка Ehya";
+$body = "
+<h2>Новое подписка</h2>
+<b>Почта:</b> $email";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'akademglokovich@gmail.com'; // Логин на почте
+    $mail->Password   = 'a1539815'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('glok.akademov@gmail.com', 'Академ'); // Адрес самой почты и имя отправителя
+    $mail->SMTPKeepAlive = true;
+
+    // Получатель письма
+    $mail->addAddress('kirill87rabota@gmail.com');  
+    // $mail->addAddress('kir.ya-a@yandex.ru');
+//    Ещё один, если нужен  
+
+//     // Прикрипление файлов к письму
+// if (!empty($file['name'][0])) {
+//     for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+//         $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+//         $filename = $file['name'][$ct];
+//         if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+//             $mail->addAttachment($uploadfile, $filename);
+//             $rfile[] = "Файл $filename прикреплён";
+//         } else {
+//             $rfile[] = "Не удалось прикрепить файл $filename";
+//         }
+//     }   
+// }
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+header('location: thankyou.html');
