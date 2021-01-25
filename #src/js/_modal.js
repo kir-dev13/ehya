19153295ab9e0bfb5f009d867d4cf900e30b0eb9
kir-dev13 <kirill87rabota@gmail.com>
2021-01-26 00:31:28 +0300

    3 level done

--- #src/js/_modal.js
+++ #src/js/_modal.js
@@ -24,3 +24,17 @@ $(document).keyup(function (e) {
     closeModal();
   }
 });
+
+jQuery(function ($) {
+  $(document).mouseup(function (e) {
+    // событие клика по веб-документу
+    var div = $(".modal__dialog"); // тут указываем ID элемента
+    if (
+      !div.is(e.target) && // если клик был не по нашему блоку
+      div.has(e.target).length === 0
+    ) {
+      // и не по его дочерним элементам
+      closeModal(); // скрываем его
+    }
+  });
+});
--- dist/js/script.js
+++ dist/js/script.js
@@ -103,6 +103,20 @@ $(document).keyup(function (e) {
     closeModal();
   }
 });
+
+jQuery(function ($) {
+  $(document).mouseup(function (e) {
+    // событие клика по веб-документу
+    var div = $(".modal__dialog"); // тут указываем ID элемента
+    if (
+      !div.is(e.target) && // если клик был не по нашему блоку
+      div.has(e.target).length === 0
+    ) {
+      // и не по его дочерним элементам
+      closeModal(); // скрываем его
+    }
+  });
+});
 ;
 $(document).ready(function () {
   let tabsItem = $(".trends__menu-item");
